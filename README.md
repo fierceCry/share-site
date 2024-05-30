0abc33daa9739a4c89fc6f2ce4958b5a08ac0fa1
kimmangyu <vlsual0917@gmail.com>
2024-05-30 18:32:20 +0900

    chore: 프로젝트 초기 설정 완료
    프로젝트의 초기 설정을 완료했습니다.
    - 주요 라이브러리 및 패키지 설치
    - 기본 폴더 구조 설정

---
+++ README.md
@@ -0,0 +1,4 @@
+## 실행방법
+-- 배포용 node src/app.js
+-- 개발용 nodemon app.js
+
---
+++ src/routers/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const apiRouter = express.Router();
+
+// apiRouter.use('/auth', authRouter);
+// apiRouter.use('/users', usersRouter);
+// apiRouter.use('/posts', postsRouter);
+
+export { apiRouter };
