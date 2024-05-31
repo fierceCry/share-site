6168ea95f0dad8c2213c28b1de2b6107ce299de6
leeminjun <mlnsuk0702@gmail.com>
2024-05-31 21:30:22 +0900

    pull 저장

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-const main = async() =>{
+const main = async () => {
   const categories = [
     { regionName: '수도권' },
     { regionName: '충청권' },
@@ -16,10 +16,10 @@ const main = async() =>{
   }
 
   console.log('seed 완료!!');
-}
+};
 
 main()
-  .catch(e => {
+  .catch((e) => {
     console.error(e);
     process.exit(1);
   })
