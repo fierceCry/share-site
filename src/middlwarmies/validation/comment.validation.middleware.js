b541ad4de420c69ea8acf98390548c7eac737496
SW-64 <ettjddns6@gmail.com>
2024-06-03 00:08:40 +0900

    RefreshToken 및 로그아웃 API 구현

--- src/middlwarmies/validation/comment.validation.middleware.js
+++ src/middlwarmies/validation/comment.validation.middleware.js
@@ -1,11 +1,11 @@
-import Joi from 'joi';
+import Joi from 'Joi';
 
 /** 댓글 기능 joi **/
 export const commentSchema = async (req, res, next) => {
   try {
     const commentSchema = Joi.object({
-      comment : Joi.string().required().empty('').messages({
-        'any.required':  ':댓글 입력해주세요.',
+      comment: Joi.string().required().empty('').messages({
+        'any.required': ':댓글 입력해주세요.',
       })
     });
     await commentSchema.validateAsync(req.body);
