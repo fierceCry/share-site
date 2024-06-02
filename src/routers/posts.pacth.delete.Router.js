f33ae1d19e49a72a9220586b517cb37c729bfeed
leeminjun <mlnsuk0702@gmail.com>
2024-06-02 16:52:58 +0900

    feat:수정 기능 추가

---
+++ src/routers/posts.pacth.delete.Router.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import { prisma } from '../utils/prisma.utils.js';
+// import bcrypt from 'bcrypt';
+// import jwt from 'jsonwebtoken';
+//미들웨어 파일 필요
+
+const router = express.Router();
+
+// 게시글 수정
+router.patch(
+  '/posts/:id',
+  /* 미들웨어,*/ async (req, res, next) => {
+    const { userId } = req.user;
+    const { id } = req.params;
+    const { title, content, imageUrl, regionName } = req.body;
+
+    const idcheck = await prisma.post.findFirst({
+      where: { AND: [{ userId: +userId }, { postId: +id }] },
+    });
+
+    if (!idcheck) {
+      return res.status(400).json({ message: '게시물이 존재하지 않습니다.' });
+    }
+
+    if (!title && !content && !imageUrl && !regionName) {
+      return res.status(400).json({ message: '수정된 내용이 없습니다.' });
+    }
+
+    const updatedPost = await prisma.post.update({
+      where: { postId: +id },
+      data: {
+        title,
+        content,
+        imageUrl,
+        regionName,
+        updatedAt: new Date(),
+      },
+      select: {
+        postId: true,
+        userId: true,
+        title: true,
+        content: true,
+        imageUrl: true,
+        regionName: true, // users 테이블의 createdAt 필드 선택
+        createdAt: true, // users 테이블의 createdAt 필드 선택
+        updatedAt: true, // users 테이블의 updatedAt 필드 선택
+      },
+    });
+
+    return res.status(200).json({
+      status: 200,
+      message: '수정완료 되었습니다.',
+      data: updatedResume,
+    });
+  }
+);
