633f76e98f8d35b740f864056ccb0bdec13b561c
Parkseojin2 <bym401543@gmail.com>
2024-06-03 11:56:49 +0900

    Update comment.validation.middleware.js

--- src/middlwarmies/validation/comment.validation.middleware.js
+++ src/middlwarmies/validation/comment.validation.middleware.js
@@ -4,8 +4,8 @@ import Joi from 'joi';
 export const commentSchema = async (req, res, next) => {
   try {
     const commentSchema = Joi.object({
-      comment : Joi.string().required().empty('').messages({
-        'any.required':  ':댓글 입력해주세요.',
+      comment: Joi.string().required().empty('').messages({
+        'any.required': ':댓글 입력해주세요.',
       })
     });
     await commentSchema.validateAsync(req.body);
