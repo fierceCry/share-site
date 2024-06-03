16ee204e656953310a22123b1a983cea5ea76f4e
Parkseojin2 <bym401543@gmail.com>
2024-06-03 11:57:00 +0900

    Update index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { authRouter } from './auth.router.js';
+//import { postsRouter } from './posts.router.js';
 import { postsRouter } from './post.router.js';
 import { profileRouter } from './profile.router.js';
 
@@ -9,5 +10,6 @@ apiRouter.use('/auth', authRouter);
 apiRouter.use('/posts', postsRouter);
 apiRouter.use('/profile', profileRouter);
 // apiRouter.use('/users', usersRouter);
+// apiRouter.use('/posts', postsRouter);
 
 export { apiRouter };
