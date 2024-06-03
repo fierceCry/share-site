211fa8f83e740186da660a0009acbb8f83b8b403
Parkseojin2 <bym401543@gmail.com>
2024-06-03 15:22:22 +0900

    Update auth.router.js

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -12,7 +12,7 @@ import { MESSAGES } from '../constants/message.constant.js';
 import { signupValidator } from '../middlwarmies/validation/sign-up-validator.middleware.js';
 import { signinValidator } from '../middlwarmies/validation/sign-in-validator.middleware.js';
 import { requireRefreshToken } from '../middlwarmies/require-refresh-token.middleware.js';
-
+import kakaoStrategy from '../passport.Strategy/kakao.Strategy.js';
 const authRouter = express();
 
 authRouter.get(
@@ -28,6 +28,21 @@ authRouter.get(
     res.json({ data });
   }
 );
+authRouter.get('/kakao', kakaoStrategy.authenticate('kakao', { session: false, authType: 'reprompt' }));
+
+//? 위에서 카카오 서버 로그인이 되면, 카카오 redirect url 설정에 따라 이쪽 라우터로 오게 된다.
+authRouter.get(
+  '/kakao/callback',
+  //? 그리고 passport 로그인 전략에 의해 kakaoStrategy로 가서 카카오계정 정보와 DB를 비교해서 회원가입시키거나 로그인 처리하게 한다.
+  kakaoStrategy.authenticate('kakao', { session: false, failureRedirect: '/' }),
+  // kakaoStrategy에서 성공한다면 콜백 실행
+  (req, res) => {
+    const data = req.user.data;
+    res.json({ data });
+  },
+);
+
+
 
 authRouter.post('/sign-up', signupValidator, async (req, res, next) => {
   try {
