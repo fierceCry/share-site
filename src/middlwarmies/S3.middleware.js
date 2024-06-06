4011bb7c1c9742542a4fecdd075ecdad18e3c1ae
kimmangyu <vlsual0917@gmail.com>
2024-06-07 02:46:29 +0900

    최종 완성본

--- src/middlwarmies/S3.middleware.js
+++ src/middlwarmies/S3.middleware.js
@@ -5,17 +5,26 @@ import multer from 'multer';
 import multerS3 from 'multer-s3';
 import AWS from 'aws-sdk';
 import path from 'path';
+import { S3Client } from "@aws-sdk/client-s3";
 
-const s3 = new AWS.S3({
+
+const s3Client = new S3Client({
   region: process.env.AWS_S3_REGION,
-  accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+  credentials: {
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+  },
 });
+// const s3 = new AWS.S3({
+//   region: process.env.AWS_S3_REGION,
+//   accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+//   secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+// });
 
 // 프로필 S3
 const profileUpload = multer({
   storage: multerS3({
-    s3: s3,
+    s3: s3Client,
     bucket: process.env.AWS_S3_BUCKET,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, callback) => {
@@ -33,7 +42,7 @@ const profileUpload = multer({
 // 게시물 S3
 const postUpload = multer({
   storage: multerS3({
-    s3: s3,
+    s3: s3Client,
     bucket: process.env.AWS_S3_BUCKET,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, callback) => {
